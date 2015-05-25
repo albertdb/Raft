e9232ec62b08dc5bfae8be65b2ea78b226e3b40a
albertdb <albert@ANTISPAM.duatonet.es>
2015-05-25 02:31:28 +0200

    Implementing replication

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ var id=process.argv[2];
     currentTerm=0,
     state='f',
     votedFor=null,
+    log=[null],
+    commitIndex=0,
+    lastApplied=0,
+    nextIndex=Object.create(null),
+    matchIndex=Object.create(null),
     grantedVotes=0,
     electionTime=randomInt(1500, 3000),
     heartbeatTime=750,
-    serversIDs=Object.create(null),
     zmq=require('zmq'),
     socket = zmq.socket('dealer');
     
@@ -17,9 +21,11 @@ function sendMessage(destination,message){
 
 var electionTimer=setTimeout(electionTimeout,electionTime);
 var heartbeatTimer;
+var newEntryInterval=setInterval(newEntry,1000);
 
 for(var i=0; i<4; i++){
-    if(i!=id) serversIDs[i]=true;
+    if(i!=id) nextIndex[i]=1;
+    if(i!=id) matchIndex[i]=0;
 }
 
 socket.on('message',function(){
@@ -29,6 +35,7 @@ socket.on('message',function(){
     showArguments(args);
     var message=JSON.parse(args[3]);
     if(message.rpc=='appendEntries') appendEntries(message.term,message.leaderId,message.prevLogIndex,message.prevLogTerm,message.entries,message.leaderCommit);
+    else if(message.rpc=='replyAppendEntries') replyAppendEntries(message.term,message.success);
     else if(message.rpc=='requestVote') requestVote(message.term,message.candidateId,message.lastLogIndex,message.lastLogTerm);
     else if(message.rpc=='replyVote') replyVote(message.term,message.voteGranted);
     }
@@ -39,6 +46,7 @@ sendMessage(process.argv[4],'Hola');
 //RPCs
 
 function appendEntries(term,leaderId,prevLogIndex,prevLogTerm,entries,leaderCommit){
+    var message;
     if(term>=currentTerm){
         if(term>currentTerm){
             /*Term evolution
@@ -46,9 +54,37 @@ function appendEntries(term,leaderId,prevLogIndex,prevLogTerm,entries,leaderComm
             for(var i=currentTerm+1;i<term;i++) process.stdout.write(' ');*/
             currentTerm=term;
         }
-        console.log('Reseteo timer');
+        if(log[prevLogIndex].term==prevLogTerm){
+            for(var entry in entries) log.push(entry);
+            message=JSON.stringify({rpc: 'replyAppendEntries', term: currentTerm, success: true});
+            if(leaderCommit>commitIndex) commitIndex=Math.min(leaderCommit,log.length-1);
+        }
+        else{
+            while(prevLogIndex<log.length) log.pop();
+            message=JSON.stringify({rpc: 'replyAppendEntries', term: currentTerm, success: false});
+        }
         clearTimeout(electionTimer);
         electionTimer=setTimeout(electionTimeout,electionTime);
+    }
+    else message=JSON.stringify({rpc: 'replyAppendEntries', term: currentTerm, success: false});
+    sendMessage(leaderId,message);
+}
+
+function replyAppendEntries(term,success){
+    if(term>currentTerm){
+        /*Term evolution
+        process.stdout.write(state);
+        for(var i=currentTerm+1;i<term;i++) process.stdout.write(' ');*/
+        currentTerm=term;
+        state='f';
+        grantedVotes=0;
+        votedFor=null;
+    }
+    else if(success){
+        
+    }
+    else{
+        
     }
 }
 
@@ -86,10 +122,15 @@ function replyVote(term,voteGranted){
     }
     else if(voteGranted && term==currentTerm){
         grantedVotes++;
-        if(grantedVotes>(Object.keys(serversIDs).length+1)/2){
+        if(grantedVotes>(Object.keys(nextIndex).length+1)/2){
             //console.log("Election win");
             state='l';
             grantedVotes=0;
+            for (var i in nextIndex) {
+                (function(serverId){
+                    nextIndex[serverId]=log.length;
+                })(i);
+            }
             heartbeatTimer=setTimeout(heartbeatTimeout,0);
             //NO! votedFor=null;
         }
@@ -105,7 +146,7 @@ function electionTimeout(){
 		state='c';
 		votedFor=id;
 		grantedVotes=1
-		for (var i in serversIDs) {
+		for (var i in nextIndex) {
         (function(serverId){
             var message=JSON.stringify({rpc: 'requestVote', term: currentTerm, candidateId: id});
             sendMessage(serverId,message);
@@ -116,9 +157,9 @@ function electionTimeout(){
 }
 
 function heartbeatTimeout(){
-		for (var i in serversIDs) {
+		for (var i in nextIndex) {
         (function(serverId){
-            var message=JSON.stringify({rpc: 'appendEntries', term: currentTerm, leaderId: id, prevLogIndex: null, prevLogTerm: null,entries: null, leaderCommit: null});
+            var message=JSON.stringify({rpc: 'appendEntries', term: currentTerm, leaderId: id, prevLogIndex: log.length-1, prevLogTerm: log[log.length-1].term,entries: null, leaderCommit: commitIndex});
             sendMessage(serverId,message);
         })(i);
 		}
@@ -127,6 +168,31 @@ function heartbeatTimeout(){
 		clearTimeout(electionTimer);
     electionTimer=setTimeout(electionTimeout,electionTime);
 }
+
+function newEntry(){
+    if(state=='l'){
+      var entry=new LogEntry((new Date()).toISOString(),currentTerm);
+      for (var i in nextIndex) {
+          (function(serverId){
+              var message=JSON.stringify({rpc: 'appendEntries', term: currentTerm, leaderId: id, prevLogIndex: log.length-1, prevLogTerm: log[log.length-1].term,entries: [entry], leaderCommit: commitIndex});
+              sendMessage(serverId,message);
+          })(i);
+      }
+      log.push(entry);
+      clearTimeout(heartbeatTimer);
+      heartbeatTimer=setTimeout(heartbeatTimeout,heartbeatTime);
+      clearTimeout(electionTimer);
+      electionTimer=setTimeout(electionTimeout,electionTime);
+    }
+}
+
+//Internal classes
+
+function LogEntry(command,term){
+    this.command=command;
+    this.term=term;
+}
+
 
 //Aux functions
 function showArguments(a) {
