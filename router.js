968d22216cd25179852f92f16e209f0857f73c03
albertdb <albert@ANTISPAM.duatonet.es>
2015-07-28 01:46:08 +0200

    Add debug command-line option to router

--- router.js
+++ router.js
@@ -1,11 +1,12 @@
-var zmq=require('zmq')
+var debug=process.argv[3]==true,
+    zmq=require('zmq'),
     router=zmq.socket('router');
 
 router.bindSync('tcp://*:'+process.argv[2]);
 
 router.on('message',function(){
     var args = Array.apply(null, arguments);
-    showArguments(args);
+    if(debug) showArguments(args);
     router.send([args[2],'',args[0],'',args[4]]);
 });
 
